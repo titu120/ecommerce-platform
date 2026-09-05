ac4cd47d5a7d7618350884f95b68acf8951a7cde
Titufrk <titufrk@gmail.com>
2026-09-05 23:23:56 +0600

    11: Create the IProductService interface

---
+++ Ecommerce.Application/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Application.DTOs.Product;
+
+namespace Ecommerce.Application.Interfaces;
+
+public interface IProductService
+{
+    Task<ProductDto> CreateProductAsync(CreateProductDto dto);
+    Task<ProductDto> UpdateProductAsync(int id, UpdateProductDto dto);
+    Task DeleteProductAsync(int id);
+    Task<ProductDto> GetProductByIdAsync(int id);
+    Task<IEnumerable<ProductDto>> GetAllProductsAsync(int pageNumber, int pageSize);
+    Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId);
+    Task<IEnumerable<ProductDto>> SearchProductsAsync(string keyword);
+    Task<IEnumerable<ProductDto>> GetLowStockProductsAsync(int threshold);
+}
