a98214613abe8e4304e2cffb6854d978c29f24fe
Titufrk <titufrk@gmail.com>
2026-09-05 13:42:58 +0600

    8: Install AutoMapper and create MappingProfile

---
+++ Ecommerce.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // এখানে পরে DTO তৈরি হলে CreateMap() যোগ করবো
+            // যেমন: CreateMap<Product, ProductDto>();
+            //       CreateMap<CreateProductDto, Product>();
+        }
+    }
+}
