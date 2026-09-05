a5fe0ccda193c1dca637604ef5422a86edb365e8
Titufrk <titufrk@gmail.com>
2026-09-05 23:26:40 +0600

    12: Create the ProductService class implementing IProductService

---
+++ Ecommerce.Application/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Ecommerce.Application.DTOs.Product;
+using Ecommerce.Application.Interfaces;
+
+namespace Ecommerce.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<ProductDto> CreateProductAsync(CreateProductDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> UpdateProductAsync(int id, UpdateProductDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetAllProductsAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> SearchProductsAsync(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetLowStockProductsAsync(int threshold)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
