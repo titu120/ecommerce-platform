1abd8b26d95ffc7a90cf9ddf351efff6f37f69d9
Titufrk <titufrk@gmail.com>
2026-09-05 13:29:20 +0600

    7: Create IUnitOfWork interface

---
+++ Ecommerce.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.Application.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IProductRepository Products { get; }
+    ICategoryRepository Categories { get; }
+    IOrderRepository Orders { get; }
+    ICartRepository Carts { get; }
+    IUserRepository Users { get; }
+
+    Task<int> SaveChangesAsync();
+}
