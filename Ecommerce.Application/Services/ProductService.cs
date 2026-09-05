b0ab74cbd5a8f813035c84832ede09701a383efc
Titufrk <titufrk@gmail.com>
2026-09-05 23:35:00 +0600

    17: Validation Service layer এ বসান

--- Ecommerce.Application/Services/ProductService.cs
+++ Ecommerce.Application/Services/ProductService.cs
@@ -1,54 +1,75 @@
 ﻿using Ecommerce.Application.DTOs.Product;
 using Ecommerce.Application.Interfaces;
+using FluentValidation;
 
-namespace Ecommerce.Application.Services;
-
-public class ProductService : IProductService
+namespace Ecommerce.Application.Services
 {
-    private readonly IUnitOfWork _unitOfWork;
-
-    public ProductService(IUnitOfWork unitOfWork)
+    public class ProductService : IProductService
     {
-        _unitOfWork = unitOfWork;
-    }
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<CreateProductDto> _createValidator;
+        private readonly IValidator<UpdateProductDto> _updateValidator;
 
-    public Task<ProductDto> CreateProductAsync(CreateProductDto dto)
-    {
-        throw new NotImplementedException();
-    }
+        public ProductService(
+            IUnitOfWork unitOfWork,
+            IValidator<CreateProductDto> createValidator,
+            IValidator<UpdateProductDto> updateValidator)
+        {
+            _unitOfWork = unitOfWork;
+            _createValidator = createValidator;
+            _updateValidator = updateValidator;
+        }
 
-    public Task<ProductDto> UpdateProductAsync(int id, UpdateProductDto dto)
-    {
-        throw new NotImplementedException();
-    }
+        public async Task<ProductDto> CreateProductAsync(CreateProductDto dto)
+        {
+            var validationResult = await _createValidator.ValidateAsync(dto);
+            if (!validationResult.IsValid)
+            {
+                throw new Exceptions.ValidationException(validationResult.Errors);
+            }
 
-    public Task DeleteProductAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+            throw new NotImplementedException(); // আসল logic পরের ধাপে (E4) আসবে
+        }
 
-    public Task<ProductDto> GetProductByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+        public async Task<ProductDto> UpdateProductAsync(int id, UpdateProductDto dto)
+        {
+            var validationResult = await _updateValidator.ValidateAsync(dto);
+            if (!validationResult.IsValid)
+            {
+                throw new Exceptions.ValidationException(validationResult.Errors);
+            }
 
-    public Task<IEnumerable<ProductDto>> GetAllProductsAsync(int pageNumber, int pageSize)
-    {
-        throw new NotImplementedException();
-    }
+            throw new NotImplementedException(); // আসল logic পরের ধাপে (E4) আসবে
+        }
 
-    public Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId)
-    {
-        throw new NotImplementedException();
-    }
+        public Task DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
 
-    public Task<IEnumerable<ProductDto>> SearchProductsAsync(string keyword)
-    {
-        throw new NotImplementedException();
-    }
+        public Task<ProductDto> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
 
-    public Task<IEnumerable<ProductDto>> GetLowStockProductsAsync(int threshold)
-    {
-        throw new NotImplementedException();
+        public Task<IEnumerable<ProductDto>> GetAllProductsAsync(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> SearchProductsAsync(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDto>> GetLowStockProductsAsync(int threshold)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Ecommerce.Application/Validators/UpdateProductDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Application.DTOs.Product;
+using FluentValidation;
+
+namespace Ecommerce.Application.Validators
+{
+    public class UpdateProductDtoValidator : AbstractValidator<UpdateProductDto>
+    {
+        public UpdateProductDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .MaximumLength(150).WithMessage("Product name cannot exceed 150 characters.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0.");
+
+            RuleFor(x => x.StockQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("Stock quantity cannot be negative.");
+
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("A valid CategoryId is required.");
+        }
+    }
+}
