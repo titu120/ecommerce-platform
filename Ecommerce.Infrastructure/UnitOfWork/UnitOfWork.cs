f29970c03885930ad579078eb5bfe84f005c5e02
Titufrk <titufrk@gmail.com>
2026-09-06 11:04:29 +0600

    25: Implement UnitOfWork and SaveChangesAsync()

---
+++ Ecommerce.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Ecommerce.Application.Interfaces;
+using Ecommerce.Infrastructure.Persistence;
+using Ecommerce.Infrastructure.Repositories;
+
+namespace Ecommerce.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        private IProductRepository? _products;
+        private ICategoryRepository? _categories;
+        private IOrderRepository? _orders;
+        private ICartRepository? _carts;
+        private IUserRepository? _users;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProductRepository Products =>
+            _products ??= new ProductRepository(_context);
+
+        public ICategoryRepository Categories =>
+            _categories ??= new CategoryRepository(_context);
+
+        public IOrderRepository Orders =>
+            _orders ??= new OrderRepository(_context);
+
+        public ICartRepository Carts =>
+            _carts ??= new CartRepository(_context);
+
+        public IUserRepository Users =>
+            _users ??= new UserRepository(_context);
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
