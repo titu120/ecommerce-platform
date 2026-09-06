3202305b1ac2f58e6ef442d205b37bdc060755c9
Titufrk <titufrk@gmail.com>
2026-09-06 11:16:58 +0600

    32: Create the ProductsController and inject IProductService

---
+++ Ecommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Ecommerce.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+    }
+}
